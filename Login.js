0fe5034c5b6477099c14c862ff57ad46e112abe8
Gokulnath <rgokulnath24@gmail.com>
2025-09-01 14:18:10 +0530

    solve the issue

--- Login.js
+++ Login.js
@@ -1,7 +1,3 @@
 function greet() {
-<<<<<<< HEAD
     console.log("Hello from Updated DEV and Feature branch!");
-=======
-    console.log("Hello from FEATURE branch!");
->>>>>>> 5b9291e (Login JS Files are added)
 }
